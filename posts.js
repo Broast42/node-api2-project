add30e3a07b456318b8896f71bab0ec6a7072210
rdowd <rjarrettdowd@yahoo.com>
2020-04-10 10:59:05 -0400

    added all routes and tested them in insomnia.

--- posts.js
+++ posts.js
@@ -64,7 +64,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req,res) =>{
     db.findById(req.params.id)
         .then(post => {
-            if(!post.length === 0){
+            if(post.length !== 0){
                 res.status(200).json(post);
             }else{
                 res.status(404).json({
@@ -81,6 +81,91 @@ router.get("/:id", (req,res) =>{
         })
         
 });
+
+router.get("/:id/comments", (req, res) => {
+    db.findPostComments(req.params.id)
+        .then(comment => {
+            if(comment.length !== 0){
+                res.status(200).json(comment);
+            }else{
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."  
+                })
+            }
+            
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500),json({
+                error: "The comments information could not be retrieved." 
+            })
+        })
+});
+
+router.delete("/:id", (req, res) => {
+    // let postToDelete = [];
+    db.findById(req.params.id)
+        .then(post => {
+            // postToDelete = post; 
+            if (post.length !== 0){
+                db.remove(req.params.id)
+                    .then(nums => {
+                        res.status(200).json({nums})
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: "The post could not be removed"
+                        })
+                    });
+            }else{
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist." 
+                })
+            }   
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: "The post could not be removed"
+            })
+        });
+            
+});
+
+router.put("/:id", (req, res) => {
+    db.findById(req.params.id)
+        .then(post =>{
+            if(post.length !== 0){
+                if(!req.body.title || !req.body.contents){
+                    res.status(400).json({
+                        errorMessage: "Please provide title and contents for the post." 
+                    })
+                }else{
+                    db.update(req.params.id, req.body)
+                        .then(update =>{
+                            res.status(200).json(update);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: "The post information could not be modified."
+                            })
+                        })
+                }
+            }else{
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                }) 
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: "The post information could not be modified."
+            })
+        })
+})  
 
 
 module.exports = router;
